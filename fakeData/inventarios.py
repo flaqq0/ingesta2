ca3c323770e554a3bf020c72aef3a4ffc500cb4d
flaqq0 <flavia.honores@utec.edu.pe>
2024-12-02 02:01:36 -0500

    para wong

--- fakeData/inventarios.py
+++ fakeData/inventarios.py
@@ -9,7 +9,7 @@ from decimal import Decimal  # Importar Decimal para DynamoDB
 fake = Faker()
 
 # Lista de tenants
-tenants = "uwu" #, "wong", "plazavea"]
+tenants = "wong" #, "uwu", "plazavea"]
 
 # Salida
 output_file_inventories = "inventarios.json"
