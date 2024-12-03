e9b0155e34e36cb5982fa6f1c3db6de6c0e8aee6
flaqq0 <flavia.honores@utec.edu.pe>
2024-12-03 02:49:10 -0500

    logs

--- ingesta/t_usuarios/load_usuarios.py
+++ ingesta/t_usuarios/load_usuarios.py
@@ -7,16 +7,15 @@ from datetime import datetime
 
 # Configuración del logger
 LOG_FILE_PATH = "./logs/load_usuarios.log"
-logger.add(LOG_FILE_PATH, format="{time:YYYY-MM-DD HH:mm:ss.SSS} | {level} | {message}", level="INFO", rotation="10 MB")
+logger.add(LOG_FILE_PATH, format="{time:2024-11-30 HH:mm:ss.SSS} | {level} | {message}", level="INFO", rotation="10 MB")
 
 # Variables globales
-BASE_DIRECTORY = "../../fakeData"  # Directorio base donde se encuentran los archivos locales
-BUCKET_NAME = "aproyecto-dev"  # Bucket S3 donde se subirán los datos
+BASE_DIRECTORY = "./exported_data"
+BUCKET_NAME = "aproyecto-dev"
 
 # Conexión a S3
-s3 = boto3.client('s3')
+s3 = boto3.client("s3")
 
-# Función para verificar la existencia del bucket
 def check_bucket_exists(bucket_name):
     try:
         s3.head_bucket(Bucket=bucket_name)
@@ -26,7 +25,6 @@ def check_bucket_exists(bucket_name):
         logger.error(f"Error al acceder al bucket '{bucket_name}': {str(e)}")
         return False
 
-# Función para subir un archivo a S3
 def upload_to_s3(file_path, bucket, s3_file_path):
     try:
         s3.upload_file(file_path, bucket, s3_file_path)
@@ -40,11 +38,8 @@ def upload_to_s3(file_path, bucket, s3_file_path):
     except Exception as e:
         logger.error(f"Error desconocido al subir el archivo '{file_path}': {str(e)}")
 
-# Función principal para realizar la ingesta
 def ingest():
     logger.info(f"Iniciando ingesta al bucket '{BUCKET_NAME}'.")
-    
-    # Verificar si el bucket existe
     if not check_bucket_exists(BUCKET_NAME):
         logger.critical(f"El bucket '{BUCKET_NAME}' no está disponible. Abortando ingesta.")
         return
@@ -52,19 +47,16 @@ def ingest():
     start_time = datetime.now()
     processed_files = 0
 
-    # Verificar si el directorio BASE_DIRECTORY existe y contiene archivos
     if not os.path.exists(BASE_DIRECTORY):
         logger.error(f"El directorio '{BASE_DIRECTORY}' no existe. Abortando ingesta.")
         return
 
-    # Buscar el archivo productos.json en el directorio
     file_path = os.path.join(BASE_DIRECTORY, "usuarios.json")
     if not os.path.isfile(file_path):
         logger.warning(f"No se encontró el archivo 'usuarios.json' en '{BASE_DIRECTORY}'. Nada para subir.")
         return
 
-    # Subir archivo al bucket S3
-    s3_file_path = "usuarios/usuarios.json"  # Ruta en el bucket S3
+    s3_file_path = "usuarios/usuarios.json"
     try:
         logger.info(f"Procesando archivo: {file_path}")
         upload_to_s3(file_path, BUCKET_NAME, s3_file_path)
